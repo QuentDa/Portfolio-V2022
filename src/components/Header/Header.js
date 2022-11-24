a9864541719b57bff9119a9f947b15ec6a3b3629
Quentin De Andrade <deandradeq@gmail.com>
2022-11-24 03:49:16 +0100

    Architecture projet

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../../logo.svg'
+import './Header.css'
+
+export default function Header() {
+    return(
+        <header className="Header">
+            logo
+        </header>
+    );
+}
